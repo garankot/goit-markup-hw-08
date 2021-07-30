91abe380960217f106e78d1fff9a27267600e672
G_T <garanko.t@gmail.com>
2021-07-31 02:39:51 +0300

    + header

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  
-    mobileBtnClose.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
